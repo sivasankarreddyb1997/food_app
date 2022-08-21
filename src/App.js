c75f6f535d14d925d151a28842f5de006bf18d3e
sivasankarreddy0 <59655611+sivasankarreddy0@users.noreply.github.com>
2022-08-21 21:02:06 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./styles.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
+  const fetchData = () => {
+    // console.log("testing the data");
+    axios.get("https://dummyjson.com/products?limit=10").then((res) => {
+      setData(res.data.products);
+    });
+  };
+
+  useEffect(() => {
+    if (count % 2 === 0) {
+      fetchData();
+    } else {
+      setData([]);
+      // alert("error in API please try again");
+    }
+  }, [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Count: {count} </h1> <br />
+      <button className="btn" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>{" "}
+      &nbsp;
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <br />
+      {data.map((dat) => (
+        <li key={dat.id}>
+          {dat.title}-{dat.price}
+        </li>
+      ))}
+    </>
   );
-}
+};
 
 export default App;
